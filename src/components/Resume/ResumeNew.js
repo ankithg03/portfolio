a080b9cda8555b4a40d40e84f5d478dc9f143570
Ankith G <ankithg@corra.com>
2022-10-02 16:10:51 +0530

    code

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/ankithg03/portfolio/master/src/Assets/AnkithG_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/ankithg03/portfolio/master/src/Assets/Ankith.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
