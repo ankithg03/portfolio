78d075c1251ccceac9dd25748bfa51f0a51a65e1
Ankith G <corra-337@corra-337.local>
2025-05-18 21:05:07 +0530

    added product card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -19,7 +19,12 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     <div className={clsx("rounded-lg shadow-md p-4 flex flex-col", s['project-card'])}>
       <img src={project.image} alt={project.name} className={clsx(s['project-image'], "w-full h-48 object-cover")} />
       <div className='mt-4 mb-auto text-center font-PPMori'>
-        <h2 className="text-lg font-bold mb-auto text-[#3b3b3b]">{project.name}</h2>
+        <h2 className={
+            clsx(
+                "text-lg font-bold mb-auto",
+                s["project-title"]
+            )
+        }>{project.name}</h2>
         <p className="text-gray-500 text-sm">{project.description}</p>
         <div className={s['projects']}>
             {project?.stack?.map((tech, index) => (
@@ -28,12 +33,12 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         </div>
         <div className='flex gap-2 text-center justify-center'>
             {project.github && (
-                <><a href={project.github} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline font-bold">
+                <><a href={project.github} target="_blank" rel="noopener noreferrer" className="text-blue-500 font-PPMori-bold hover:underline font-bold">
                 View on GitHub
                 </a>{project?.link ? " | " : ""}</>
             )}
             {project.link && (
-                <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline font-bold">
+                <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 font-PPMori-bold  hover:underline font-bold">
                 Live Demo
                 </a>
             )}
